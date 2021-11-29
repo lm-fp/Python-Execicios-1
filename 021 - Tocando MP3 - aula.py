25c2e4052cacadafee9aff6e69bbdf7f44bc36c5
Leticia Maria <lm.pires0.16@gmail.com>
2021-11-28 23:08:32 -0300

    ex 020 e 021

---
+++ 021 - Tocando MP3 - aula.py
@@ -0,0 +1,8 @@
+'''O codigo que foi mostrado na aula
+porem nao funcionou'''
+
+import pygame
+pygame.init()
+pygame.mixer.music.load('021.mp3')
+pygame.mixer.music.play()
+pygame.event.wait()
---
+++ 021 - Tocando MP3 - ok.py
@@ -0,0 +1,8 @@
+'''Essa foi a forma que conseguir reproduzir
+ a que foi mostrada na aula nao deu certo'''
+ 
+import os, time
+'''r = caminho absoluto''' 
+os.startfile(r'021.mp3')
+'''time=tempo e  sleep=dormir parar a execução'''
+time.sleep(10)

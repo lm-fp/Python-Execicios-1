9aa0fbc542520074ba53bb3b8f648f5827db0abc
Leticia Maria <lm.pires0.16@gmail.com>
2021-12-01 21:32:51 -0300

    pequenas modificações no ex 026 e 027

--- 026 - Primeira e ultima ocorrencia de uma string.py
+++ 026 - Primeira e ultima ocorrencia de uma string.py
@@ -1,5 +1,4 @@
-nome = str(input('Digite uma frase motivadora: '))
-nome = nome.lower()
+nome = str(input('Digite uma frase motivadora: ')).strip().lower()
 print('A letra a aparece:',nome.count('a'), 'vezes')
-print('A letra "A" aparece na primeira posição:'.format(nome.find('a')))
-print('A letra "A" aparece na ultima posição:'.format(nome.rfind('a')))
+print('A letra "A" aparece na primeira posição: {}'.format(nome.find('a')+1))
+print('A letra "A" aparece na ultima posição: {}'.format(nome.rfind('a')+1))
--- 027 - Primeiro e ultimo nome de uma pessoa.py
+++ 027 - Primeiro e ultimo nome de uma pessoa.py
@@ -1,3 +1,4 @@
-nome = str(input('Digite seu nome completo: '))
+nome = str(input('Digite seu nome completo: ')).strip()
 nome = nome.split()
 print('Primeiro nome:',nome[0])
+print('Ultimo nome: ',nome[-1])

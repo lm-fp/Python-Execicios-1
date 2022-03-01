5e53971f0f7f6e4df021664ca3969a8f3941d7f0
Leticia Maria <lm.pires0.16@gmail.com>
2022-03-01 20:21:17 -0300

    12a

---
+++ ex - aulas/aula12a.py
@@ -0,0 +1,13 @@
+# condições aninhadas
+#if elif else
+
+nome = str(input("Qual seu nome? "))
+
+if nome == 'Leticia':
+    print("Que nome lindo!")
+elif nome == 'Ana maria':
+    print('Nome normal!')
+elif nome in 'carla bruna silva':
+    print("nome diferente")
+
+print("tenha um bom dia,",nome)
